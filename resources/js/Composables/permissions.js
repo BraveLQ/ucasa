038d2b45a6f3aa86b1f9e25f269b3c0e2c7a16eb
loiq.amonbekov <amonbekov.loiq@gmail.com>
2023-03-01 16:55:13 +0500

    Added Composable permissions.js in App

--- resources/js/Composables/permissions.js
+++ resources/js/Composables/permissions.js
@@ -1,1 +1,9 @@
 import {usePage} from "@inertiajs/vue3";
+
+export function usePermission(){
+    const hasRole = (name)=> usePage().props.auth.user.roles.include(name);
+    const hasPermission = (name)=> usePage().props.auth.user.include(name);
+
+    return{hasRole, hasPermission}
+
+}
